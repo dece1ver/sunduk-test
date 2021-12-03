dd427cb4236ac59597219e2abc74902ab5703960
dece1ver <36793765+dece1ver@users.noreply.github.com>
2021-12-03 13:41:43 +0000

    Deploying to gh-test-pages from @ dece1ver/Sunduk@e7e380b8de91ad42b878667c89299a67cf8cc2f3 🚀

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -201,7 +201,7 @@
       "url": "_framework\/icudt.dat"
     },
     {
-      "hash": "sha256-eHBLMiJRT4Bef\/eXGwl09gebmm7UvwyxtcTGIvQUMCs=",
+      "hash": "sha256-wtqd0t3WMAuPhwMzCf8WJacAZkXb1HXxO07u2pIE+AI=",
       "url": "_framework\/blazor.boot.json"
     },
     {
@@ -281,7 +281,7 @@
       "url": "_framework\/netstandard.dll"
     },
     {
-      "hash": "sha256-bBZevvwDKUsZiRdT\/NuMHlgHDTQXRcTAJfkS7ZFfFsk=",
+      "hash": "sha256-ze5vcsgFoN9z3u9kkuMHf2V42EPRFI7O1qBJcvTM9xg=",
       "url": "_framework\/Sunduk.dll"
     },
     {
@@ -409,5 +409,5 @@
       "url": "_content\/MudBlazor\/MudBlazor.min.js"
     }
   ],
-  "version": "GTnTe1Tf"
+  "version": "T9XJVSGj"
 };
--- service-worker.js
+++ service-worker.js
@@ -46,4 +46,4 @@ async function onFetch(event) {
 
     return cachedResponse || fetch(event.request);
 }
-/* Manifest version: GTnTe1Tf */
+/* Manifest version: T9XJVSGj */
