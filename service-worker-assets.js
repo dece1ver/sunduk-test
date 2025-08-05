0774d6a1922cfc157939b31feeec5ff31949e97f
dece1ver <v.dece1verr@gmail.com>
2025-08-05 13:19:11 +0000

    Deploying to gh-test-pages from @ dece1ver/Sunduk@47fdf6e37e2f88083a36711996d83af2a87e34eb 🚀

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -1,5 +1,5 @@
 self.assetsManifest = {
-  "version": "jrIorg0o",
+  "version": "rE/vGdgk",
   "assets": [
     {
       "hash": "sha256-IUupasMjnt+uyy0AMhIXLvfJPM6HiK4w3xtmE8SMHak=",
@@ -226,8 +226,8 @@ self.assetsManifest = {
       "url": "_framework/Parlot.e0bk6br4se.wasm"
     },
     {
-      "hash": "sha256-8e6gI1Tbg6eGjUiey1sitrKd1A7uGB7fJh0pDXb48OM=",
-      "url": "_framework/Sunduk.n6ikkt5082.wasm"
+      "hash": "sha256-5zorrfXudPNv977cmUNFfCCFsFjqgPTCGD27ERGC3HQ=",
+      "url": "_framework/Sunduk.3h4lhgkkof.wasm"
     },
     {
       "hash": "sha256-pVYXVmD3yiBwzyz9nJsDKcDn+qq4r72Hf4g0ddi6dec=",
@@ -878,7 +878,7 @@ self.assetsManifest = {
       "url": "_framework/WindowsBase.pg3o6xzigg.wasm"
     },
     {
-      "hash": "sha256-YoOs2///rny4HGoGHuo/q5ZQIoRB1nZk6Z0tqRGaHvM=",
+      "hash": "sha256-Hc5UHdf1Bh/3dd0G9DwEkOz+tGvHZwn3uxNw76R2ixM=",
       "url": "_framework/blazor.boot.json"
     },
     {
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: jrIorg0o */
+/* Manifest version: rE/vGdgk */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
