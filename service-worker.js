4ee8f242731a3be26d7ca5f4c4bb1b60dc16ed91
dece1ver <v.dece1verr@gmail.com>
2026-04-03 06:50:51 +0000

    Deploying to gh-test-pages from @ dece1ver/Sunduk@e2ba60ebac9c03b7cc94c67b06e8243f3bc7063e 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: znQZTVC+ */
+/* Manifest version: gHcQrQik */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
